c3c4e357e409dd9836a5d2f71e73d5cbc13b6bfc
u19028386 <102651202+u19028386@users.noreply.github.com>
2022-06-07 19:03:29 +0200

    new changes

---
+++ WebApplication1/Models/FileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class FileModel
+    {
+        // what will be displayed
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+    }
+}
